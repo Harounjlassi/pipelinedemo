1f412cdfd8f4fb918d6e3a95d36a6a10ffc187b4
unknown <jlassi.haroun@esprit.tn>
2023-06-16 15:04:43 +0100

    codeDemo

---
+++ server/src/main/java/com/example/Greeter.java
@@ -0,0 +1,20 @@
+package com.example;
+
+/**
+ * This is a test class.
+ * We are testing again
+ */
+public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+
+  }
+ public String greet(String someone) {
+  //TODO: Add javadoc comment
+    //return String.format("Hello ?, %s!", someone);
+   return String.format("Hello There, %s!", someone);
+  }
+}
